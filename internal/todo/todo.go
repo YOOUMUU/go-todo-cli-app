cf8f1a93a2ff5cadece8dbbfd3c3fa1293b3ccb5
YOOUMUU <yooumuu@gmail.com>
2024-08-28 11:51:00 +0800

    setup todo.go

---
+++ internal/todo/todo.go
@@ -0,0 +1,94 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"time"
+)
+
+type todoItem struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type Todos []todoItem
+
+func (t *Todos) Add(task string) {
+	todo := todoItem{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(id int) error {
+	tasksList := *t
+	if id <= 0 || id > len(tasksList) {
+		return errors.New("invaild id")
+	}
+
+	tasksList[id-1].CompletedAt = time.Now()
+	tasksList[id-1].Done = true
+
+	return nil
+}
+
+func (t *Todos) UnComplete(id int) error {
+	tasksList := *t
+	if id <= 0 || id > len(tasksList) {
+		return errors.New("invaild id")
+	}
+
+	tasksList[id-1].CompletedAt = time.Time{}
+	tasksList[id-1].Done = false
+
+	return nil
+}
+
+func (t *Todos) Delete(id int) error {
+	tasksList := *t
+	if id <= 0 || id > len(tasksList) {
+		return errors.New("invaild id")
+	}
+
+	*t = append(tasksList[:id-1], tasksList...)
+
+	return nil
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return err
+	}
+
+	err = json.Unmarshal(file, t)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Todos) Store(filename string) error {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, data, 0644)
+}
